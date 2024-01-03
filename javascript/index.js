fdd8b21b3748056b2a55a7038c6b6d83d50f7566
nmrndll <81620556+nmrndll@users.noreply.github.com>
2024-01-03 19:20:32 +0800

    update function

---
+++ javascript/index.js
@@ -0,0 +1,52 @@
+function updateTime() {
+  let londonElement = document.querySelector("#London");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("MMMM	Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let perthElement = document.querySelector("#Perth");
+  if (perthElement) {
+    let perthDateElement = perthElement.querySelector(".date");
+    let perthTimeElement = perthElement.querySelector(".time");
+    let perthTime = moment().tz("Australia/Perth");
+
+    perthDateElement.innerHTML = perthTime.format("MMMM	Do YYYY");
+    perthTimeElement.innerHTML = perthTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
